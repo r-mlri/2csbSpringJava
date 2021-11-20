123b96105de4d3d4795551e3a682751732368e56
r-mlri <rafael.mallari.cics@ust.edu.ph>
2021-11-20 09:35:20 +0800

    initial spring commit

---
+++ src/main/java/com/csb/SampleFullstack/SampleFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.csb.SampleFullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleFullstackApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/csb/SampleFullstack/controller/Greet.java
@@ -0,0 +1,10 @@
+package com.csb.SampleFullstack.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping
+public class Greet {
+    
+}
