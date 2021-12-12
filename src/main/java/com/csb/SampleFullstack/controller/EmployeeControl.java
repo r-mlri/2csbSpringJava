fbf6d8ab13b59d7007994b668208ca9be2f0848a
r-mlri <rafael.mallari.cics@ust.edu.ph>
2021-12-13 07:07:00 +0800

    Update EmployeeControl.java

--- src/main/java/com/csb/SampleFullstack/controller/EmployeeControl.java
+++ src/main/java/com/csb/SampleFullstack/controller/EmployeeControl.java
@@ -1,18 +1,16 @@
 package com.csb.SampleFullstack.controller;
 
-import com.csb.SampleFullstack.Employee;
-import com.csb.SampleFullstack.EmployeeRepo;
+import com.csb.SampleFullstack.employee.Employee;
+import com.csb.SampleFullstack.repository.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/employee")
 @CrossOrigin("*")
-
 public class EmployeeControl {
     @Autowired
     private EmployeeRepo employeeRepo;
@@ -22,5 +20,25 @@ public class EmployeeControl {
         return employeeRepo.findAll();
     };
 
+    @PostMapping("/employees")
+    public Employee postEmployee(@RequestBody Employee employee){
+        return employeeRepo.save(employee);
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployeeById(@PathVariable Long id){
+        return employeeRepo.findById(id).get();
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee){
+        return employeeRepo.save(employee);
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<HttpStatus> deleteEmployeeById(@PathVariable Long id){
+        employeeRepo.deleteById(id);
+        return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
+    }
 
 }
